cefde251f3e9ca16db10a66d7474d570622cb5a8
NevoRaz1 <nevoraz07@gmail.com>
2026-08-12 12:04:10 +0300

    i added check for the flag so the mines dont touch it

--- board.py
+++ board.py
@@ -1,18 +1,18 @@
 import random
-import consts
-from consts import BOARD_ROWS,BOARD_COLUMNS,GROUND,MINE
+from consts import BOARD_ROWS,BOARD_COLUMNS,GROUND,MINE,FLAG
 board=[]
-for row in range(0, 25):
+for row in range(BOARD_ROWS):
     board.append([])
-    for column in range(0, 50):
+    for column in range(BOARD_COLUMNS):
         board[row].append(GROUND)
+print(len(board))
 
 def put_mines_in_board(board):
     mines=[]
     for i in range(20):#run 20 times for 20 mines
         row=random.randint(0,BOARD_ROWS-3)#roll random number between 0-len-3 because the mine length is 3
         col=random.randint(0,BOARD_COLUMNS-3)
-        while (row,col) in mines and (row,col+1) in mines and (row,col+2) in mines:
+        while (row,col) in mines and (row,col+1) in mines and (row,col+2) in mines and board[row][col]==FLAG and board[row][col+1]==FLAG and board[row][col+2]==FLAG :
             row = random.randint(0, BOARD_ROWS - 3)
             col = random.randint(0, BOARD_COLUMNS - 3)
         mine=[(row,col),(row,col+1),(row,col+2)]
