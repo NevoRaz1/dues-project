fd72c3ae91985116b4f3706320b61b696cec45b2
tal565 <zamirtal56@gmail.com>
2026-08-12 16:17:35 +0300

    before merge

--- board.py
+++ board.py
@@ -1,5 +1,7 @@
 import random
 from consts import BOARD_ROWS,BOARD_COLUMNS,GROUND,MINE,FLAG,SOLDIER
+
+
 def create_board():
     board=[]
     for row in range(BOARD_ROWS):
@@ -44,12 +46,7 @@ def put_mines_in_board(board):
     for mine in mines:#פה שמים את הפצצות בלוח
         for mine_square in mine:
             board[mine_square[0]][mine_square[1]]=MINE
-def where_is_soldier(board):
-    return index_list:
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col] == SOLDIER:
-        row col
+
 
 
 board=create_board()
