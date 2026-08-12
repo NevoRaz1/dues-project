6a0432f314e8235f1bf31a8da9855d746c372893
NevoRaz1 <nevoraz07@gmail.com>
2026-08-12 16:25:32 +0300

    need to work on soldier move and we need to put only two SOLDIER in the board  and to work on is board ok function

--- board.py
+++ board.py
@@ -44,12 +44,7 @@ def put_mines_in_board(board):
     for mine in mines:#פה שמים את הפצצות בלוח
         for mine_square in mine:
             board[mine_square[0]][mine_square[1]]=MINE
-# def where_is_soldier(board):
-#     return index_list:
-#     for row in range(len(board)):
-#         for col in range(len(board[row])):
-#             if board[row][col] == SOLDIER:
-#         row col
+
 def is_board_ok(board,row=0,col=0):
 
 
@@ -69,8 +64,3 @@ while not is_board_ok(board):
 add_soldier(board)
 put_flag(board)
 put_mines_in_board(board)
-
-
-
-for i in board:
-    print(i)
--- soldier.py
+++ soldier.py
@@ -1,17 +1,40 @@
-from consts import SOLDIER,BOARD_ROWS
+from consts import SOLDIER, BOARD_ROWS, MINE
+from board import board
+def where_is_soldier(board):
+    index_list=[]
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == SOLDIER:
+                    index_list.append([row,col])
+    return index_list
+
+
+
+
 def create_soldier():
     new_player=[]
     for row in range(4):
         new_player.append([])
         for column in range(2):
             new_player[row].append(SOLDIER)
-from board import where_is_soldier
 
-def player_index(row, column):
+def player_is_lose():
+    foot_list=[]
+    index_list=where_is_soldier(board)
+    row=index_list[0][0]
+    column=index_list[0][1]
+    foot_list=foot_index(row,column)
+    row1=foot_list[0][0]
+    column1=foot_list[0][1]
+    row2=foot_list[1][0]
+    column2=foot_list[1][1]
+    if board[row1][column1]==MINE or board[row2][column2]==MINE:
+        return True
+    return False
 
-def player_move(player,letter):
-    player[]
-    if letter=='a':
+
+
+
 
 
 
