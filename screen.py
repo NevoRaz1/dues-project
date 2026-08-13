2e502f358e81e10bbafb73e16c30d0d2aad055b8
NevoRaz1 <nevoraz07@gmail.com>
2026-08-13 11:46:30 +0300

    finish soldier movement

--- screen.py
+++ screen.py
@@ -24,6 +24,9 @@ screen.blit(grass_img, (random.randint(0, 100), 30))
 
 font = pygame.font.Font(None, 20)
 
+lose_font = pygame.font.Font(None, 100)
+lose_text = font.render("You Lose!",True,(255,255,255))
+
 soldier_x = where_is_soldier(board)[0][0]
 soldier_y=30
 running=True
@@ -45,16 +48,24 @@ while running:
             quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                move_soldier("s")
+                if move_soldier("s") == False:
+                    screen.blit(lose_text, (100, 100))
+                    running = False
                 soldier_y+=22.8
             elif event.key == pygame.K_UP:
-                move_soldier("w")
+                if move_soldier("w") == False:
+                    screen.blit(lose_text, (100, 100))
+                    running = False
                 soldier_y-=22.8
             elif event.key == pygame.K_LEFT:
-                move_soldier("a")
+                if move_soldier("a") == False:
+                    screen.blit(lose_text, (100, 100))
+                    running = False
                 soldier_x-=11.4
             elif event.key == pygame.K_RIGHT:
-                move_soldier("d")
+                if move_soldier("d") == False:
+                    screen.blit(lose_text, (100, 100))
+                    running = False
                 soldier_x+=11.4
 
     pygame.display.flip()
