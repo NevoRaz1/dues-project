6d34a0819098a30e4426650f993d6454ea57f2d6
NevoRaz1 <nevoraz07@gmail.com>
2026-08-13 09:04:17 +0300

    i finish the is board ok function change the soldier creation only the leggs

--- board.py
+++ board.py
@@ -8,9 +8,7 @@ def create_board():
             board[row].append(GROUND)
     return board
 def add_soldier(board):
-    for row in range(4):
-        for col in range(2):
-            board[row][col]=SOLDIER
+    board[2][1],board[2][0]=SOLDIER,SOLDIER
 def put_flag(board):
     for row in range(22,BOARD_ROWS):
         for column in range(46,BOARD_COLUMNS):
@@ -45,12 +43,18 @@ def put_mines_in_board(board):
         for mine_square in mine:
             board[mine_square[0]][mine_square[1]]=MINE
 
-def is_board_ok(board,row=0,col=0):
+def is_board_ok(board, row=2, col=0):
 
-    if board[row][col+1] != MINE and col+1<BOARD_COLUMNS-1 and board[row][col+2] != MINE:
-        return is_board_ok(board,row,col+1)
-    if board[row+1][col] != MINE and row+1<BOARD_ROWS-1:
-        return is_board_ok(board, row+1, col)
+    if board[row][col] == FLAG or board[row][col+1] == FLAG:
+        return True
+
+    if col + 2 < BOARD_COLUMNS and board[row][col+2] != MINE:
+        if is_board_ok(board, row, col + 1):
+            return True
+
+    if row + 1 < BOARD_ROWS and board[row+1][col] != MINE and board[row+1][col+1] != MINE:
+        if is_board_ok(board, row + 1, col):
+            return True
 
     return False
 
@@ -58,6 +62,13 @@ board=create_board()
 add_soldier(board)
 put_flag(board)
 put_mines_in_board(board)
+while not is_board_ok(board):
+
+    board=create_board()
+    add_soldier(board)
+    put_flag(board)
+    put_mines_in_board(board)
+
 
 
 
--- soldier.py
+++ soldier.py
@@ -1,35 +1,19 @@
-from consts import SOLDIER, BOARD_ROWS, MINE
+from consts import SOLDIER, MINE
 from board import board
 def where_is_soldier(board):
     index_list=[]
     for row in range(len(board)):
         for col in range(len(board[row])):
             if board[row][col] == SOLDIER:
-                    index_list.append([row,col])
+                    index_list.append((row,col))
     return index_list
 
 
-
-
-def create_soldier():
-    new_player=[]
-    for row in range(4):
-        new_player.append([])
-        for column in range(2):
-            new_player[row].append(SOLDIER)
-
 def player_is_lose():
-    foot_list=[]
-    index_list=where_is_soldier(board)
-    row=index_list[0][0]
-    column=index_list[0][1]
-    foot_list=foot_index(row,column)
-    row1=foot_list[0][0]
-    column1=foot_list[0][1]
-    row2=foot_list[1][0]
-    column2=foot_list[1][1]
-    if board[row1][column1]==MINE or board[row2][column2]==MINE:
-        return True
+    foot_list=where_is_soldier(board)
+    for foot in foot_list:
+        if board[foot[0]][foot[1]]==MINE :
+            return True
     return False
 
 
