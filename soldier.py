ce32663068a0c1e3438018830a2b10d67ce44b89
NevoRaz1 <nevoraz07@gmail.com>
2026-08-13 10:19:30 +0300

    got soldier movement

--- soldier.py
+++ soldier.py
@@ -1,4 +1,4 @@
-from consts import SOLDIER, MINE
+from consts import SOLDIER, MINE, FLAG,BOARD_ROWS,BOARD_COLUMNS,GROUND
 from board import board
 def where_is_soldier(board):
     index_list=[]
@@ -7,6 +7,61 @@ def where_is_soldier(board):
             if board[row][col] == SOLDIER:
                     index_list.append([row,col])
     return index_list
+def move_soldier(letter):
+    pos=where_is_soldier(board)
+    current_index1=pos[0]
+    current_index2=pos[1]
+    possible_moves=[FLAG,SOLDIER,MINE,GROUND]
+    if letter=='a':
+        if current_index1[1]!=0:
+            board[current_index1[0]][current_index1[1]-1]=SOLDIER
+            board[current_index2[0]][current_index2[1]]=GROUND
+            return True
+        else:
+            return False
+    if letter=='s':
+            if current_index1[0]!=BOARD_ROWS:
+                board[current_index1[0]+1][current_index1[1]]=SOLDIER
+                board[current_index2[0]+1][current_index2[1]]=SOLDIER
+                board[current_index1[0]][current_index1[1]]=GROUND
+                board[current_index2[0]][current_index2[1]]=GROUND
+                return True
+            else:
+                return False
+
+
+
+    if letter=='d':
+        if current_index2[1]+1!=BOARD_COLUMNS:
+            board[current_index2[0]][current_index2[1]+1]=SOLDIER
+            board[current_index1[0]][current_index1[1]]=GROUND
+            return True
+        else:
+            return False
+    if letter=='w':
+        if current_index1[0]>3:
+            if board[current_index1[0]-1][current_index1[1]] in possible_moves and board[current_index2[0]-1][current_index2[1]] in possible_moves:
+                board[current_index1[0]-1][current_index1[1]]=SOLDIER
+                board[current_index2[0]-1][current_index2[1]]=SOLDIER
+                board[current_index1[0]][current_index1[1]]=GROUND
+                board[current_index2[0]][current_index2[1]]=GROUND
+            return True
+        else:
+            return False
+
+
+letter=input("enter a,s,d,w: ")
+while move_soldier(letter):
+    for i in board:
+        print(i)
+
+    letter=input("enter a,s,d,w: ")
+
+
+
+
+
+
 
 
 def player_is_lose():
@@ -27,3 +82,4 @@ def foot_index(row, column):
     index_list.append([row+3,column])
     index_list.append([row+3,column+1])
     return index_list
+
