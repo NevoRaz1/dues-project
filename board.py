941943dbc956a484bd499016a67d074dd71d1d57
tal565 <zamirtal56@gmail.com>
2026-08-17 09:01:57 +0300

    i added the xrray function but it still need nevo save

--- board.py
+++ board.py
@@ -1,7 +1,9 @@
 import random
-from consts import BOARD_ROWS,BOARD_COLUMNS,GROUND,MINE,FLAG,SOLDIER
+from consts import BOARD_ROWS, BOARD_COLUMNS, GROUND, MINE, FLAG, SOLDIER
+
+
 def create_board():
-    board=[]
+    board = []
     for row in range(BOARD_ROWS):
         board.append([])
         for column in range(BOARD_COLUMNS):
@@ -15,57 +17,147 @@ def create_board():
         put_flag(board)
         put_mines_in_board(board)
     return board
+
+
 def add_soldier(board):
-    board[3][1],board[3][0]=SOLDIER,SOLDIER
+    board[3][1], board[3][0] = SOLDIER, SOLDIER
+
+
 def put_flag(board):
-    for row in range(22,BOARD_ROWS):
-        for column in range(46,BOARD_COLUMNS):
-            board[row][column]=FLAG
-def is_mine_exist(mines,row,col):
+    for row in range(22, BOARD_ROWS):
+        for column in range(46, BOARD_COLUMNS):
+            board[row][column] = FLAG
+
+
+def is_mine_exist(mines, row, col):
     for mine in range(len(mines)):
-        if mines[mine][0]==(row,col) or (row,col) == mines[mine][1] or (row,col) == mines[mine][2]:
+        if mines[mine][0] == (row, col) or (row, col) == mines[mine][1] or (row, col) == mines[mine][2]:
             return True
     return False
+
+
 def put_mines_in_board(board):
-    mines=[]
+    mines = []
     row = random.randint(0, BOARD_ROWS - 1)  # roll random number between 0-len-3 because the mine length is 3
     col = random.randint(0, BOARD_COLUMNS - 3)
-    while board[row][col] == SOLDIER or board[row][col+1] == SOLDIER or board[row][col+2] == SOLDIER or  board[row][col] == FLAG or board[row][col + 1] == FLAG or board[row][col + 2] == FLAG:
+    while board[row][col] == SOLDIER or board[row][col + 1] == SOLDIER or board[row][col + 2] == SOLDIER or board[row][
+        col] == FLAG or board[row][col + 1] == FLAG or board[row][col + 2] == FLAG:
         row = random.randint(0, BOARD_ROWS - 3)
         col = random.randint(0, BOARD_COLUMNS - 3)
     mine = [(row, col), (row, col + 1), (row, col + 2)]
     mines.append(mine)
-    for i in range(19):#run 20 times for 20 mines
-        row=random.randint(0,BOARD_ROWS-1)#roll random number between 0-len-3 because the mine length is 3
-        col=random.randint(0,BOARD_COLUMNS-3)
-        #checks edge cases
-        while  is_mine_exist(mines,row,col) or board[row][col] == SOLDIER or board[row][col+1] == SOLDIER or board[row][col+2] == SOLDIER  or board[row][col]==FLAG or board[row][col+1]==FLAG or board[row][col+2]==FLAG :
+    for i in range(19):  # run 20 times for 20 mines
+        row = random.randint(0, BOARD_ROWS - 1)  # roll random number between 0-len-3 because the mine length is 3
+        col = random.randint(0, BOARD_COLUMNS - 3)
+        # checks edge cases
+        while is_mine_exist(mines, row, col) or board[row][col] == SOLDIER or board[row][col + 1] == SOLDIER or \
+                board[row][col + 2] == SOLDIER or board[row][col] == FLAG or board[row][col + 1] == FLAG or board[row][
+            col + 2] == FLAG:
             row = random.randint(0, BOARD_ROWS - 3)
             col = random.randint(0, BOARD_COLUMNS - 3)
-        mine=[(row,col),(row,col+1),(row,col+2)]
+        mine = [(row, col), (row, col + 1), (row, col + 2)]
         mines.append(mine)
-    #mines=לרשימה שיש בה רשימות[[(),(),()],[(),(),()],[(),(),()],[(),(),()]]
-    #mine=[(),(),()]
-    #mine_square=()
-    for mine in mines:#פה שמים את הפצצות בלוח
+        # mines=לרשימה שיש בה רשימות[[(),(),()],[(),(),()],[(),(),()],[(),(),()]]
+    # mine=[(),(),()]
+    # mine_square=()
+    for mine in mines:  # פה שמים את הפצצות בלוח
         for mine_square in mine:
-            board[mine_square[0]][mine_square[1]]=MINE
-def is_board_ok(board, row=2, col=0):
+            board[mine_square[0]][mine_square[1]] = MINE
 
-    if board[row][col] == FLAG or board[row][col+1] == FLAG:
+
+def is_board_ok(board, row=2, col=0):
+    if board[row][col] == FLAG or board[row][col + 1] == FLAG:
         return True
 
-    if col + 2 < BOARD_COLUMNS and board[row][col+2] != MINE:
+    if col + 2 < BOARD_COLUMNS and board[row][col + 2] != MINE:
         if is_board_ok(board, row, col + 1):
             return True
 
-    if row + 1 < BOARD_ROWS and board[row+1][col] != MINE and board[row+1][col+1] != MINE:
+    if row + 1 < BOARD_ROWS and board[row + 1][col] != MINE and board[row + 1][col + 1] != MINE:
         if is_board_ok(board, row + 1, col):
             return True
 
     return False
 
 
-# board = create_board()
-# for i in board:
-#     print(i)
+
+
+
+# ===== תוספת בלבד =====
+import pygame
+
+
+def draw_board_image(board):
+    pygame.init()
+
+    screen = pygame.display.set_mode((640, 640))
+    pygame.display.set_caption("The Flag Game")
+
+    cell_width = 640 / BOARD_COLUMNS
+    cell_height = 640 / BOARD_ROWS
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+
+        for row in range(BOARD_ROWS):
+            for column in range(BOARD_COLUMNS):
+
+                x = int(column * cell_width)
+                y = int(row * cell_height)
+
+                if board[row][column] == MINE:
+                    pygame.draw.rect(
+                        screen,
+                        (255, 0, 0),
+                        (x + 2, y + 2, int(cell_width) - 4, int(cell_height) - 4)
+                    )
+
+        for column in range(BOARD_COLUMNS + 1):
+            x = int(column * cell_width)
+
+            pygame.draw.line(
+                screen,
+                (0, 100, 0),
+                (x, 0),
+                (x, 640),
+                3
+            )
+
+            pygame.draw.line(
+                screen,
+                (0, 255, 0),
+                (x, 0),
+                (x, 640),
+                1
+            )
+
+        for row in range(BOARD_ROWS + 1):
+            y = int(row * cell_height)
+
+            pygame.draw.line(
+                screen,
+                (0, 100, 0),
+                (0, y),
+                (640, y),
+                3
+            )
+
+            pygame.draw.line(
+                screen,
+                (0, 255, 0),
+                (0, y),
+                (640, y),
+                1
+            )
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+# ===== סוף התוספת =====
--- soldier.py
+++ soldier.py
@@ -1,90 +1,108 @@
-from consts import SOLDIER, MINE, FLAG,BOARD_ROWS,BOARD_COLUMNS,GROUND
+from consts import SOLDIER, MINE, FLAG, BOARD_ROWS, BOARD_COLUMNS, GROUND
 
 def where_is_soldier(board):
-    index_list=[]
+    """מחזיר את רשימת המשבצות [row, col] שבהן ממוקם החייל"""
+    index_list = []
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLUMNS):
             if board[row][col] == SOLDIER:
-                    index_list.append([row,col])
+                index_list.append([row, col])
     return index_list
-def can_soldier_move(board,letter):
-    index_list=where_is_soldier(board)
-    left_foot=index_list[0]
-    right_foot=index_list[1]
-    if letter=='a':
-        if left_foot[1] != 0:
-            return True
 
-        return False
-    if letter=='d':
-        if right_foot[1] != BOARD_COLUMNS-1:
-            return True
-        return False
-    if letter=='w':
-        if right_foot[0]>3:
-            return True
-        return False
-    if letter=='s':
-        if left_foot[0] != BOARD_ROWS-1:
-            return True
+def can_soldier_move(board, letter):
+    """בודק אם החייל יכול לזוז מבחינת גבולות הלוח"""
+    pos = where_is_soldier(board)
+    if len(pos) < 2:
         return False
 
-def move_soldier(board,letter):
+    left_foot = pos[0]
+    right_foot = pos[1]
 
-    pos=where_is_soldier(board)
-    left_foot=pos[0]
-    right_foot=pos[1]
-    possible_moves=[FLAG,SOLDIER,MINE,GROUND]
-    if letter=='a':
+    if letter == 'a':
+        return left_foot[1] > 0
+    elif letter == 'd':
+        return right_foot[1] < BOARD_COLUMNS - 1
+    elif letter == 'w':
+        return left_foot[0] > 0
+    elif letter == 's':
+        return left_foot[0] < BOARD_ROWS - 1
 
-        if board[left_foot[0]][left_foot[1]-1]==MINE:
+    return False
+
+def is_mine(board, row, col):
+    """
+    בודק בדק דיוק אם המשבצת הספציפית [row, col] היא חלק ממוקש.
+    מונע פסילה שגויה במדרכה נקייה ליד מוקש.
+    """
+    if row < 0 or row >= BOARD_ROWS or col < 0 or col >= BOARD_COLUMNS:
+        return False
+
+    # 1. בדיקה ישירה אם המשבצת עצמה מוגדרת כמוקש
+    if board[row][col] == MINE:
+        return True
+
+    # 2. בדיקה אם המשבצת משמאל היא תחילתו של מוקש (משבצת 2 של מוקש רחב)
+    if col - 1 >= 0 and board[row][col - 1] == MINE:
+        if col - 2 < 0 or board[row][col - 2] != MINE:
+            return True
+
+    # 3. בדיקה אם המשבצת שתי משבצות משמאל היא תחילתו של מוקש (משבצת 3 של מוקש רחב)
+    if col - 2 >= 0 and board[row][col - 2] == MINE:
+        if col - 3 < 0 or board[row][col - 3] != MINE:
+            return True
+
+    return False
+
+def move_soldier(board, letter):
+    """
+    מזיז את החייל במטריצה.
+    מחזיר False רק אם החייל באמת דרך על מוקש, אחרת True.
+    """
+    pos = where_is_soldier(board)
+    if len(pos) < 2:
+        return True
+
+    left_foot = pos[0]   # [row, col_left]
+    right_foot = pos[1]  # [row, col_right]
+
+    row = left_foot[0]
+    c_left = left_foot[1]
+    c_right = right_foot[1]
+
+    if letter == 'a':
+        new_col = c_left - 1
+        if is_mine(board, row, new_col):
             return False
-        board[left_foot[0]][left_foot[1]-1]=SOLDIER
-        board[right_foot[0]][right_foot[1]]=GROUND
 
+        board[row][new_col] = SOLDIER
+        board[row][c_right] = GROUND
 
-    if letter=='s':
-
-        if board[left_foot[0]+1][left_foot[1]]==FLAG or board[right_foot[0]+1][right_foot[1]]==FLAG:
-            return True
-        if board[left_foot[0]][left_foot[1]+1]==MINE or board[right_foot[0]][right_foot[1]+1]==MINE:
+    elif letter == 'd':
+        new_col = c_right + 1
+        if is_mine(board, row, new_col):
             return False
 
-        board[left_foot[0]+1][left_foot[1]]=SOLDIER
-        board[right_foot[0]+1][right_foot[1]]=SOLDIER
-        board[left_foot[0]][left_foot[1]]=GROUND
-        board[right_foot[0]][right_foot[1]]=GROUND
+        board[row][new_col] = SOLDIER
+        board[row][c_left] = GROUND
 
-    # [3,0]
-    # [3,1]
-    if letter=='d':
-         if board[right_foot[0]][right_foot[1]+1]==FLAG:
-            return True
+    elif letter == 'w':
+        new_row = row - 1
+        if is_mine(board, new_row, c_left) or is_mine(board, new_row, c_right):
+            return False
 
-         if right_foot[1]+1!=BOARD_COLUMNS and board[right_foot[0]][right_foot[1]+1]==MINE:
-             return False
-         board[right_foot[0]][right_foot[1]+1]=SOLDIER
-         board[left_foot[0]][left_foot[1]]=GROUND
+        board[new_row][c_left] = SOLDIER
+        board[new_row][c_right] = SOLDIER
+        board[row][c_left] = GROUND
+        board[row][c_right] = GROUND
 
-    if letter=='w':
+    elif letter == 's':
+        new_row = row + 1
+        if is_mine(board, new_row, c_left) or is_mine(board, new_row, c_right):
+            return False
 
-            if  board[left_foot[0]+1][left_foot[1]]==MINE and board[right_foot[0]+1][right_foot[1]]==MINE:
-                return False
+        board[new_row][c_left] = SOLDIER
+        board[new_row][c_right] = SOLDIER
+        board[row][c_left] = GROUND
+        board[row][c_right] = GROUND
 
-            board[left_foot[0]-1][left_foot[1]]=SOLDIER
-            board[right_foot[0]-1][right_foot[1]]=SOLDIER
-            board[left_foot[0]][left_foot[1]]=GROUND
-            board[right_foot[0]][right_foot[1]]=GROUND
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return True
