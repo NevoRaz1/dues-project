d991df9dfcb1d30806fecb8ea890d4ae7e5edf92
Zoori Raz <nevoraz07@gmail.com>
2026-08-18 09:57:01 +0300

    push

--- Database.py
+++ Database.py
@@ -6,6 +6,7 @@ from pathlib import Path
 def add_game_to_save(game_save,slot):
     # File path
 
+    dict={slot-48:game_save}
 
     # File path
     a = Path(f"game_save_{slot-48}.csv")
@@ -13,19 +14,24 @@ def add_game_to_save(game_save,slot):
     # Check if the file exists
     if a.exists():
 
-        with open(f'game_save_{slot-48}.csv', 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=game_save)
-            writer.writeheader()
-            writer.writerows(game_save)
+        with open(f"game_save_{slot-48}.csv", "w", newline="") as f:
+            w = csv.DictWriter(f, dict.keys())
+            w.writeheader()
+            w.writerow(dict)
 
 
 
     else:
-        df = pd.DataFrame(game_save)
+        df = pd.DataFrame(dict)
 
         csv_file_path = f'game_save_{slot-48}.csv'
 
         df.to_csv(csv_file_path, index=False)
+
+        with open(f"game_save_{slot-48}.csv", "w", newline="") as f:
+            w = csv.DictWriter(f, dict.keys())
+            w.writeheader()
+            w.writerow(dict)
 
 
 
