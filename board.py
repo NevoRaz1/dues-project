695271e9bd12924c06cd4d04a0f9d462f59857e0
Zoori Raz <nevoraz07@gmail.com>
2026-08-18 17:12:18 +0300

    after merge

--- board.py
+++ board.py
@@ -1,77 +1,84 @@
 import random
-from consts import BOARD_ROWS, BOARD_COLUMNS, GROUND, MINE, FLAG, SOLDIER, GUARD_ROW,GUARD
+from consts import BOARD_ROWS, BOARD_COLUMNS, GROUND, MINE, FLAG, SOLDIER, GUARD_ROW, GUARD, TELEPORT
+from teleport import create_teleport
 
 
 def create_board():
-    board=[]
+    board = []
     for row in range(BOARD_ROWS):
         board.append([])
         for column in range(BOARD_COLUMNS):
             board[row].append(GROUND)
+
     add_guard(board)
     add_soldier(board)
     put_flag(board)
+    create_teleport(board)
     put_mines_in_board(board)
+
     while not is_board_ok(board):
-        board = create_board()
+        board = []
+        for row in range(BOARD_ROWS):
+            board.append([])
+            for column in range(BOARD_COLUMNS):
+                board[row].append(GROUND)
         add_guard(board)
         add_soldier(board)
         put_flag(board)
+        create_teleport(board)
         put_mines_in_board(board)
     return board
+
+
 def add_guard(board):
-    board[GUARD_ROW][0]=GUARD
+    board[GUARD_ROW][0] = GUARD
+
+
 def add_soldier(board):
-    board[3][1],board[3][0]=SOLDIER,SOLDIER
+    board[3][1], board[3][0] = SOLDIER, SOLDIER
+
+
 def put_flag(board):
-    for row in range(22,BOARD_ROWS):
-        for column in range(46,BOARD_COLUMNS):
-            board[row][column]=FLAG
-def is_mine_exist(mines,row,col):
+    for row in range(22, BOARD_ROWS):
+        for column in range(46, BOARD_COLUMNS):
+            board[row][column] = FLAG
+
+
+def is_mine_exist(mines, row, col):
     for mine in range(len(mines)):
-        if mines[mine][0]==(row,col) or (row,col) == mines[mine][1] or (row,col) == mines[mine][2]:
+        if mines[mine][0] == (row, col) or (row, col) == mines[mine][1] or (row, col) == mines[mine][2]:
             return True
     return False
+
+
 def put_mines_in_board(board):
-    mines=[]
-    row = random.randint(0, BOARD_ROWS - 1)  # roll random number between 0-len-3 because the mine length is 3
-    col = random.randint(0, BOARD_COLUMNS - 3)
-    while board[row][col] == SOLDIER or board[row][col] == GUARD or board[row][col+1] == SOLDIER or board[row][col+2] == SOLDIER or  board[row][col] == FLAG or board[row][col + 1] == FLAG or board[row][col + 2] == FLAG:
-        row = random.randint(0, BOARD_ROWS - 3)
+    mines = []
+    for i in range(20):
+        row = random.randint(0, BOARD_ROWS - 1)
         col = random.randint(0, BOARD_COLUMNS - 3)
-    mine = [(row, col), (row, col + 1), (row, col + 2)]
-    mines.append(mine)
-    for i in range(19):#run 20 times for 20 mines
-        row=random.randint(0,BOARD_ROWS-1)#roll random number between 0-len-3 because the mine length is 3
-        col=random.randint(0,BOARD_COLUMNS-3)
-        #checks edge cases
-        while  is_mine_exist(mines,row,col) or board[row][col]==GUARD or board[row][col] == SOLDIER or board[row][col+1] == SOLDIER or board[row][col+2] == SOLDIER  or board[row][col]==FLAG or board[row][col+1]==FLAG or board[row][col+2]==FLAG :
+
+        while (board[row][col] in [SOLDIER, GUARD, FLAG, TELEPORT] or
+               board[row][col + 1] in [SOLDIER, FLAG, TELEPORT] or
+               board[row][col + 2] in [SOLDIER, FLAG, TELEPORT] or
+               is_mine_exist(mines, row, col)):
             row = random.randint(0, BOARD_ROWS - 3)
             col = random.randint(0, BOARD_COLUMNS - 3)
-        mine=[(row,col),(row,col+1),(row,col+2)]
+
+        mine = [(row, col), (row, col + 1), (row, col + 2)]
         mines.append(mine)
-    #mines=לרשימה שיש בה רשימות[[(),(),()],[(),(),()],[(),(),()],[(),(),()]]
-    #mine=[(),(),()]
-    #mine_square=()
-    for mine in mines:#פה שמים את הפצצות בלוח
+
+    for mine in mines:
         for mine_square in mine:
-            board[mine_square[0]][mine_square[1]]=MINE
+            board[mine_square[0]][mine_square[1]] = MINE
+
+
 def is_board_ok(board, row=2, col=0):
-
-    if board[row][col] == FLAG or board[row][col+1] == FLAG:
+    if board[row][col] == FLAG or board[row][col + 1] == FLAG:
         return True
-
-    if col + 2 < BOARD_COLUMNS and board[row][col+2] != MINE:
+    if col + 2 < BOARD_COLUMNS and board[row][col + 2] != MINE:
         if is_board_ok(board, row, col + 1):
             return True
-
-    if row + 1 < BOARD_ROWS and board[row+1][col] != MINE and board[row+1][col+1] != MINE:
+    if row + 1 < BOARD_ROWS and board[row + 1][col] != MINE and board[row + 1][col + 1] != MINE:
         if is_board_ok(board, row + 1, col):
             return True
-
     return False
-
-
-# board = create_board()
-# for i in board:
-#     print(i)
--- soldier.py
+++ soldier.py
@@ -1,90 +1,105 @@
-from consts import SOLDIER, MINE, FLAG,BOARD_ROWS,BOARD_COLUMNS,GROUND
+from consts import (
+    BOARD_COLUMNS,
+    BOARD_ROWS,
+    FLAG,
+    GROUND,
+    MINE,
+    SOLDIER,
+    TELEPORT,
+    GUARD
+)
+from teleport import get_teleport_list
 
 def where_is_soldier(board):
-    index_list=[]
-    for row in range(BOARD_ROWS):
+    for row in range(BOARD_ROWS - 1, -1, -1):
         for col in range(BOARD_COLUMNS):
             if board[row][col] == SOLDIER:
-                    index_list.append([row,col])
-    return index_list
-def can_soldier_move(board,letter):
-    index_list=where_is_soldier(board)
-    left_foot=index_list[0]
-    right_foot=index_list[1]
-    if letter=='a':
-        if left_foot[1] != 0:
-            return True
+                return [(row, col), (row, col + 1)]
+    return [(3, 0), (3, 1)]
 
+def can_soldier_move(board, direction):
+    legs = where_is_soldier(board)
+    bottom_row, left_col = legs[0]
+    top_row = bottom_row - 3
+    right_col = legs[1][1]
+
+    if direction == "w" and top_row > 0:
+        return True
+    if direction == "s" and bottom_row < BOARD_ROWS - 1:
+        return True
+    if direction == "a" and left_col > 0:
+        return True
+    if direction == "d" and right_col < BOARD_COLUMNS - 1:
+        return True
+    return False
+
+def set_soldier_position(board, bottom_row, left_col):
+    for r in range(bottom_row - 3, bottom_row + 1):
+        board[r][left_col] = SOLDIER
+        board[r][left_col + 1] = SOLDIER
+
+def clear_soldier(board):
+    """מוחק את החייל (ומחזיר טלפורטים אם איכשהו דרכו עליהם)."""
+    teleport_cells = set()
+    for t_row, t_col in get_teleport_list():
+        teleport_cells.add((t_row, t_col))
+        teleport_cells.add((t_row, t_col + 1))
+        teleport_cells.add((t_row, t_col + 2))
+
+    for r in range(BOARD_ROWS):
+        for c in range(BOARD_COLUMNS):
+            if board[r][c] == SOLDIER:
+                if (r, c) in teleport_cells:
+                    board[r][c] = TELEPORT
+                else:
+                    board[r][c] = GROUND
+
+def move_soldier(board, direction):
+    legs = where_is_soldier(board)
+    bottom_row, left_col = legs[0]
+
+    next_bottom = bottom_row
+    next_left = left_col
+
+    if direction == "w":
+        next_bottom -= 1
+    elif direction == "s":
+        next_bottom += 1
+    elif direction == "a":
+        next_left -= 1
+    elif direction == "d":
+        next_left += 1
+
+    next_right = next_left + 1
+
+    # חסימה: אם הצעד הבא של החייל פוגע בטלפורט, התנועה מבוטלת!
+    if (
+        board[next_bottom][next_left] == TELEPORT
+        or board[next_bottom][next_right] == TELEPORT
+    ):
+        return None  # החייל נחסם, אז לא מעדכנים מיקום ופשוט ממשיכים כרגיל
+
+    # 1. בדיקת מוקש או התנגשות בשומר
+    if (
+        board[next_bottom][next_left] == MINE
+        or board[next_bottom][next_right] == MINE
+        or board[next_bottom][next_left] == GUARD
+        or board[next_bottom][next_right] == GUARD
+    ):
+        clear_soldier(board)
+        set_soldier_position(board, next_bottom, next_left)
         return False
-    if letter=='d':
-        if right_foot[1] != BOARD_COLUMNS-1:
-            return True
-        return False
-    if letter=='w':
-        if right_foot[0]>3:
-            return True
-        return False
-    if letter=='s':
-        if left_foot[0] != BOARD_ROWS-1:
-            return True
-        return False
 
-def move_soldier(board,letter):
+    # 2. בדיקת דגל
+    if (
+        board[next_bottom][next_left] == FLAG
+        or board[next_bottom][next_right] == FLAG
+    ):
+        clear_soldier(board)
+        set_soldier_position(board, next_bottom, next_left)
+        return True
 
-    pos=where_is_soldier(board)
-    left_foot=pos[0]
-    right_foot=pos[1]
-    possible_moves=[FLAG,SOLDIER,MINE,GROUND]
-    if letter=='a':
-
-        if board[left_foot[0]][left_foot[1]-1]==MINE:
-            return False
-        board[left_foot[0]][left_foot[1]-1]=SOLDIER
-        board[right_foot[0]][right_foot[1]]=GROUND
-
-
-    if letter=='s':
-
-        if board[left_foot[0]+1][left_foot[1]]==FLAG or board[right_foot[0]+1][right_foot[1]]==FLAG:
-            return True
-        if board[left_foot[0]+1][left_foot[1]]==MINE or board[right_foot[0]+1][right_foot[1]]==MINE:
-            return False
-
-        board[left_foot[0]+1][left_foot[1]]=SOLDIER
-        board[right_foot[0]+1][right_foot[1]]=SOLDIER
-        board[left_foot[0]][left_foot[1]]=GROUND
-        board[right_foot[0]][right_foot[1]]=GROUND
-
-    # [3,0]
-    # [3,1]
-    if letter=='d':
-         if board[right_foot[0]][right_foot[1]+1]==FLAG:
-            return True
-
-         if right_foot[1]+1!=BOARD_COLUMNS and board[right_foot[0]][right_foot[1]+1]==MINE:
-             return False
-         board[right_foot[0]][right_foot[1]+1]=SOLDIER
-         board[left_foot[0]][left_foot[1]]=GROUND
-
-    if letter=='w':
-
-            if  board[left_foot[0]-1][left_foot[1]]==MINE and board[right_foot[0]-1][right_foot[1]]==MINE:
-                return False
-
-            board[left_foot[0]-1][left_foot[1]]=SOLDIER
-            board[right_foot[0]-1][right_foot[1]]=SOLDIER
-            board[left_foot[0]][left_foot[1]]=GROUND
-            board[right_foot[0]][right_foot[1]]=GROUND
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # 3. תנועה רגילה
+    clear_soldier(board)
+    set_soldier_position(board, next_bottom, next_left)
+    return None
